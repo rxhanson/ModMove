168b8a06f782c703547a0d493a693c61eef6b4df
john li <jli@circularly.org>
2020-04-27 23:42:44 -0400

    change ModMove to resize from the closest corner
    Previously, ModMove always resized the window as if dragging from the bottom
    right corner. This change makes it as if the drag event occurs from whatever
    corner of the window the mouse is closest to.
    
    Unfortunately, dragging from any other corner than the bottom right is somewhat
    less precise, in that the overall position of the window can shift slightly,
    especially when dragging faster. I think this is just a consequence of the way
    the mouseMoved functions is called over and over, instead of saving the first
    and last position.

--- ModMove/Mover.swift
+++ ModMove/Mover.swift
@@ -14,7 +14,7 @@ final class Mover {
     private var lastMousePosition: CGPoint?
     private var window: AccessibilityElement?
 
-    private func mouseMoved(handler: (_ window: AccessibilityElement, _ mouseDelta: CGPoint) -> Void) {
+    private func mouseMoved(handler: (_ window: AccessibilityElement, _ mouseDelta: CGPoint, _ corner: Int) -> Void) {
         let point = Mouse.currentPosition()
         if self.window == nil {
             self.window = AccessibilityElement.systemWideElement.element(at: point)?.window()
@@ -31,23 +31,56 @@ final class Mover {
 
         window.bringToFront()
         if let lastPosition = self.lastMousePosition {
-            let mouseDelta = CGPoint(x: lastPosition.x - point.x, y: lastPosition.y - point.y)
-            handler(window, mouseDelta)
+            let mouseDelta = CGPoint(x: point.x - lastPosition.x, y: point.y - lastPosition.y)
+            handler(window, mouseDelta, closestCorner(window: window, mouse: point))
         }
 
         self.lastMousePosition = point
     }
 
-    private func resizeWindow(window: AccessibilityElement, mouseDelta: CGPoint) {
+    private func closestCorner(window: AccessibilityElement, mouse: CGPoint) -> Int {
         if let size = window.size {
-            let newSize = CGSize(width: size.width - mouseDelta.x, height: size.height - mouseDelta.y)
-            window.size = newSize
+            if let position = window.position {
+                let xmid = position.x + size.width / 2
+                let ymid = position.y + size.height / 2
+                if mouse.x < xmid && mouse.y < ymid {
+                    return 0  // top left
+                } else if mouse.x >= xmid && mouse.y < ymid {
+                    return 1  // top right
+                } else if mouse.x < xmid && mouse.y >= ymid {
+                    return 2  // bottom left
+                } else {
+                    return 3  // bottom right
+                }
+            }
+        }
+        return 3
+    }
+
+    private func resizeWindow(window: AccessibilityElement, mouseDelta: CGPoint, corner: Int) {
+        if let size = window.size {
+            if let position = window.position {
+                switch corner {
+                case 0:
+                    window.position = CGPoint(x: position.x + mouseDelta.x, y: position.y + mouseDelta.y)
+                    window.size = CGSize(width: size.width - mouseDelta.x, height: size.height - mouseDelta.y)
+                case 1:
+                    window.position = CGPoint(x: position.x, y: position.y + mouseDelta.y)
+                    window.size = CGSize(width: size.width + mouseDelta.x, height: size.height - mouseDelta.y)
+                case 2:
+                    window.position = CGPoint(x: position.x + mouseDelta.x, y: position.y)
+                    window.size = CGSize(width: size.width - mouseDelta.x, height: size.height + mouseDelta.y)
+                case 3:
+                    window.size = CGSize(width: size.width + mouseDelta.x, height: size.height + mouseDelta.y)
+                default: break
+                }
+            }
         }
     }
 
-    private func moveWindow(window: AccessibilityElement, mouseDelta: CGPoint) {
+    private func moveWindow(window: AccessibilityElement, mouseDelta: CGPoint, corner: Int) {
         if let position = window.position {
-            let newPosition = CGPoint(x: position.x - mouseDelta.x, y: position.y - mouseDelta.y)
+            let newPosition = CGPoint(x: position.x + mouseDelta.x, y: position.y + mouseDelta.y)
             window.position = newPosition
         }
     }
